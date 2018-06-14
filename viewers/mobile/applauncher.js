583bec1f5f440338efa872962783dd105ed90f60
Wilson <bianliuzhu@gmail.com>
2018-06-14 17:27:55 +0800

    init

---
+++ viewers/mobile/applauncher.js
@@ -0,0 +1,40 @@
+/** 
+ * Application launcher.
+ */
+
+// check browser support
+dwv.browser.check();
+
+// launch when page is loaded
+$(document).ready( function()
+{
+    // main application
+    var myapp = new dwv.App();
+    // initialise the application
+    myapp.init({
+        "containerDivId": "dwv",
+        "fitToWindow": true,
+        "tools": ["Scroll", "Window/Level", "Zoom/Pan", "Draw", "Livewire", "Filter"],
+        "filters": ["Threshold", "Sharpen", "Sobel"],
+        "shapes": ["Line", "Protractor", "Rectangle", "Roi", "Ellipse"],
+        "gui": ["tool", "load", "help", "undo", "version", "tags"],
+        "isMobile": true
+    });
+    
+    // example app listening
+    //var listener = function (event) { console.log("event: "+event.type); };
+    //myapp.addEventListener("load-end", listener);
+    //myapp.addEventListener("load-progress", listener);
+    //myapp.addEventListener("draw-create", listener);
+    //myapp.addEventListener("draw-move", listener);
+    //myapp.addEventListener("draw-change", listener);
+    //myapp.addEventListener("draw-delete", listener);
+    //myapp.addEventListener("wl-change", listener);
+    //myapp.addEventListener("colour-change", listener);
+    //myapp.addEventListener("position-change", listener);
+    //myapp.addEventListener("slice-change", listener);
+
+    var size = dwv.gui.getWindowSize();
+    $(".layerContainer").height(size.height);
+
+});
